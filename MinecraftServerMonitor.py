174cf3c42f9aa3d0bacf5cc4fcd0f789b81af249
OwenPendrighElliott <owenpendrighelliott@gmail.com>
2021-08-31 20:31:50 +1000

    added sounds

--- MinecraftServerMonitor.py
+++ MinecraftServerMonitor.py
@@ -1,16 +1,18 @@
 import re
 def get_updates(log_file):
     useful_lines = []
-    pat = re.compile("(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})|(\[Server thread\/INFO\])|(lost connection: Disconnected)|(<)|(Rcon)")
+    pat = re.compile(".+\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}.+")
     with open(log_file, 'r') as log:
         for line in log.read().splitlines():
-            if not pat.match(line):
+            if '[Server thread/INFO]' in line and "Rcon" not in line and "<" not in line and "lost connection: Disconnected" not in line and not pat.match(line):
                 useful_lines.append(line)
 
     print_lines = []
     with open("MC_SERVER_PREV.log", "r") as prev:
         prev_lines = prev.read()
+
         for nline in useful_lines:
+
             if nline not in prev_lines:
                 print_lines.append(nline)
 
